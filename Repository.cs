040d4c6ab70b94b6441218f8a7aae24ecae38f64
Dmitry <chumakde@gmail.com>
2020-08-22 17:34:05 +0300

    Repository (Get, GetAll)

---
+++ Repository.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace console_test
+{
+    public class Repository
+    {
+        public int Get(int id) 
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerable<int> GetAll() 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
