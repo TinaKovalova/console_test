72e6166c1dcd53bd1f5421b43f8eb64e220b9df5
Dmitry <chumakde@gmail.com>
2020-08-22 17:39:57 +0300

    Repository fixed (put, delete added)

--- Repository.cs
+++ Repository.cs
@@ -4,13 +4,21 @@ namespace console_test
 {
     public class Repository
     {
-        public int Get(int id) 
+        public int Get(int id)
         {
             throw new NotImplementedException();
         }
-        public IEnumerable<int> GetAll() 
+        public IEnumerable<int> GetAll()
         {
             throw new NotImplementedException();
+        }
+        public void Put(int value)
+        {
+            System.Console.WriteLine();
+        }
+        public void Delete(int value)
+        {
+            System.Console.WriteLine();
         }
     }
 }
