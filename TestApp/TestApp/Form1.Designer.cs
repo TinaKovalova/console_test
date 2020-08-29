ce7b33f7590622aeae607e53ec6b2efd688678b5
TinaKovalova <kovalevatkv@gmali.com>
2020-08-29 18:29:51 +0300

    Button

--- TestApp/TestApp/Form1.Designer.cs
+++ TestApp/TestApp/Form1.Designer.cs
@@ -28,13 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(27, 43);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(340, 236);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(37, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 31);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "BackColor";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Color";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(476, 100);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
     }
 }
 
--- TestApp/TestApp/Form1.cs
+++ TestApp/TestApp/Form1.cs
@@ -12,9 +12,30 @@ namespace TestApp
 {
     public partial class Form1 : Form
     {
+        RichTextBox richTextBox1;
+
+        Dictionary<string, Action<RichTextBox>> editMethods =
+            new Dictionary<string, Action<RichTextBox>>();
         public Form1()
         {
             InitializeComponent();
+            richTextBox1 = new RichTextBox();
+           
+        }
+      
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button bt = sender as Button;
+            editMethods[bt.Text](richTextBox1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (Button item in groupBox1.Controls)
+                item.Click += button_Click;
+
+            editMethods.Add("Color", (tBox) => tBox.SelectionColor = Color.Red);
+            editMethods.Add("BackColor", (tBox) => tBox.SelectionBackColor = Color.Green);
         }
     }
 }
